1cb04e1c5cfc6e1638d75c761d01e8465f793fff
Mohamed Gamil <mohamedgamil06616@gmail.com>
2026-09-11 07:42:38 +0300

    add Swagger For API Docs

---
+++ Incident intelligence platform/Incident intelligence platform/SwaggerRoleFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+public class SwaggerRoleFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        var authAttributes = context.MethodInfo
+            .GetCustomAttributes(true)
+            .OfType<AuthorizeAttribute>()
+            .Where(a => !string.IsNullOrEmpty(a.Roles));
+
+        var roles = authAttributes.SelectMany(a => a.Roles.Split(',')).Distinct().ToList();
+
+        if (roles.Any())
+        {
+            operation.Description += $"\n\n** Required Roles:** {string.Join(", ", roles)}";
+            operation.Summary = $"[{string.Join("/", roles)}] " + operation.Summary;
+        }
+    }
+}
