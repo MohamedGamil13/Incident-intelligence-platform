e83ed2747e5ad4975506d4fa2a1a674996a49ab3
Mohamed Gamil <mohamedgamil06616@gmail.com>
2026-09-12 10:48:42 +0300

    Create Incident Event Model and Make it's Config Class

---
+++ Incident intelligence platform/Incident intelligence platform/Models/IncidentEvent.cs
@@ -0,0 +1,20 @@
+﻿using Incident_intelligence_platform.Enums;
+
+namespace Incident_intelligence_platform.Models
+{
+    public class IncidentEvent
+    {
+        public int Id { get; set; }
+
+        public IncidentEventTimeStamp TimeStamp { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Title { get; set; } = string.Empty;
+
+        public DateTime Date { get; set; }
+
+        public int IncidentId { get; set; }
+    }
+
+}
---
+++ Incident intelligence platform/Incident intelligence platform/Repos/IncidentEventRepo.cs
@@ -0,0 +1,23 @@
+﻿namespace Incident_intelligence_platform.Repos
+{
+    public class IncidentEventRepo
+    {
+    }
+}
+
+//  
+/* 
+  1 -Create Model
+  2- Set Relations
+  3- Add Model To DbContext 
+  4- Create Repo to Deal with this Model
+  5- Create Service , Controller to Deal with it
+  6-Create End Points Dtos 
+  7- Register Repo and Service in Program.cs
+
+Required Endpoints : 
+
+  GET /api/incidents/{id}/timeline  
+  POST /api/incidents/{id}/events
+*/
+
