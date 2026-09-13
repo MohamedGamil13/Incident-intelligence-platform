8077f51fecb0ee4c9562866a10f113b03af6592b
Mohamed Gamil <mohamedgamil06616@gmail.com>
2026-09-13 17:41:43 +0300

    Implement UserMangement Service

--- Incident intelligence platform/Incident intelligence platform/Repos/UserMangementRepo.cs
+++ Incident intelligence platform/Incident intelligence platform/Repos/UserMangementRepo.cs
@@ -6,53 +6,70 @@ namespace Incident_intelligence_platform.Repos
 {
     public class UserMangementRepo
     {
-        private readonly AppDbcontext context;
-        private readonly UserManager<ApplicationUser> userManager;
+        private readonly AppDbcontext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
         public UserMangementRepo(AppDbcontext context, UserManager<ApplicationUser> userManager)
         {
-            this.context = context;
-            this.userManager = userManager;
-        }
-        public async Task addUser(ApplicationUser newUser)
-        {
-            await context.Users.AddAsync(newUser);
-        }
-        public void DeleteUser(ApplicationUser user)
-        {
-            context.Users.Remove(user);
-        }
-        public async Task<ApplicationUser?> GetUserData(string userId)
-        {
-            return await context.Users.FirstOrDefaultAsync(u => u.Id == userId);
-        }
-        public void UpdateUserData(ApplicationUser newUserData)
-        {
-            context.Users.Update(newUserData);
+            _context = context;
+            _userManager = userManager;
         }
 
-        public async Task<IEnumerable<ApplicationUser>> GetAllUser(int pageNumber, int pageSize)
+
+        public async Task<IdentityResult> AddUserAsync(ApplicationUser newUser, string password)
         {
-            return await context.Users
+            return await _userManager.CreateAsync(newUser, password);
+        }
+
+
+        public async Task<IdentityResult> DeleteUserAsync(ApplicationUser user)
+        {
+            return await _userManager.DeleteAsync(user);
+        }
+
+
+        public async Task<ApplicationUser?> GetUserDataAsync(string userId)
+        {
+            return await _userManager.FindByIdAsync(userId);
+        }
+
+
+        public async Task<IdentityResult> UpdateUserDataAsync(ApplicationUser user)
+        {
+            return await _userManager.UpdateAsync(user);
+
+        }
+
+
+        public async Task<IEnumerable<ApplicationUser>> GetAllUsersAsync(int pageNumber, int pageSize)
+        {
+            return await _context.Users
                 .AsNoTracking()
                 .Skip((pageNumber - 1) * pageSize)
                 .Take(pageSize)
                 .ToListAsync();
         }
 
-        public async Task<IEnumerable<ApplicationUser>> GetAllUser(int pageNumber, int pageSize, string role)
-        {
 
-            var usersInRole = await userManager.GetUsersInRoleAsync(role);
+        public async Task<IEnumerable<ApplicationUser>> GetUsersByRoleAsync(int pageNumber, int pageSize, string role)
+        {
+            var usersInRole = await _userManager.GetUsersInRoleAsync(role);
 
             return usersInRole
                 .Skip((pageNumber - 1) * pageSize)
                 .Take(pageSize)
                 .ToList();
         }
+
+        public async Task<IdentityResult> AddUserRole(ApplicationUser user, string role)
+        {
+
+            return await _userManager.AddToRoleAsync(user, role);
+        }
+
         public async Task SaveChangesAsync()
         {
-            await context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
         }
     }
 }
---
+++ Incident intelligence platform/Incident intelligence platform/Services/UserMangmentService.cs
@@ -0,0 +1,54 @@
+﻿using Incident_intelligence_platform.Models;
+using Incident_intelligence_platform.Repos;
+using Microsoft.AspNetCore.Identity;
+
+namespace Incident_intelligence_platform.Services
+{
+    public class UserMangmentService
+    {
+        private readonly UserMangementRepo userMangementRepo;
+
+
+        public UserMangmentService(UserMangementRepo userMangementRepo)
+        {
+            this.userMangementRepo = userMangementRepo;
+        }
+
+
+        public async Task<IEnumerable<ApplicationUser>> GetAllUsers(int pageNumber, int pageSize)
+        {
+            return await userMangementRepo.GetAllUsersAsync(pageNumber, pageSize);
+        }
+
+        public async Task<IEnumerable<ApplicationUser>> GetUsersByType(int pageNumber, int PageSize, string role)
+        {
+            return await userMangementRepo.GetUsersByRoleAsync(pageNumber, PageSize, role);
+        }
+
+
+        public async Task<bool> DeleteUser(string id)
+        {
+            var user = await userMangementRepo.GetUserDataAsync(id);
+            if (user == null)
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+
+        public async Task<(bool Success, string Data, string ErrorMassege, IdentityResult res)> AddUserRole(string userId, string role)
+        {
+
+            var user = await userMangementRepo.GetUserDataAsync(userId);
+
+            if (user == null)
+            {
+                return (false, string.Empty, "User not found");
+            }
+            var res = await userMangementRepo.AddUserRole(user, role);
+            return (false, string.Empty, "User Role Added Successfully", res);
+        }
+    }
+}
