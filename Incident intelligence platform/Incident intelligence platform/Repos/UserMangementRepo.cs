f048593e7fcaff32d754b2221875fe45a3f93581
Mohamed Gamil <mohamedgamil06616@gmail.com>
2026-09-13 16:13:29 +0300

    Implement UserMangment Repo

---
+++ Incident intelligence platform/Incident intelligence platform/Repos/UserMangementRepo.cs
@@ -0,0 +1,58 @@
+﻿using Incident_intelligence_platform.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Incident_intelligence_platform.Repos
+{
+    public class UserMangementRepo
+    {
+        private readonly AppDbcontext context;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserMangementRepo(AppDbcontext context, UserManager<ApplicationUser> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+        }
+        public async Task addUser(ApplicationUser newUser)
+        {
+            await context.Users.AddAsync(newUser);
+        }
+        public void DeleteUser(ApplicationUser user)
+        {
+            context.Users.Remove(user);
+        }
+        public async Task<ApplicationUser?> GetUserData(string userId)
+        {
+            return await context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        }
+        public void UpdateUserData(ApplicationUser newUserData)
+        {
+            context.Users.Update(newUserData);
+        }
+
+        public async Task<IEnumerable<ApplicationUser>> GetAllUser(int pageNumber, int pageSize)
+        {
+            return await context.Users
+                .AsNoTracking()
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<ApplicationUser>> GetAllUser(int pageNumber, int pageSize, string role)
+        {
+
+            var usersInRole = await userManager.GetUsersInRoleAsync(role);
+
+            return usersInRole
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+        }
+        public async Task SaveChangesAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+}
