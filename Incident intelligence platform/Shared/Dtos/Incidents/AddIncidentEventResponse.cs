ee81c98277f28433d8ef352f237e52996191efa5
Mohamed Gamil <mohamedgamil06616@gmail.com>
2026-09-17 12:40:11 +0300

    Ensure Every thing is Ok

---
+++ Incident intelligence platform/Shared/Dtos/Incidents/AddIncidentEventResponse.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Enums.Incident;
+
+namespace Shared.Dtos.Incidents
+{
+    public class AddIncidentEventResponse
+    {
+        public int IncidentId { get; set; }
+
+        public string IncidentName { get; set; } = string.Empty;
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public IncidentStatus IcidentStatus { get; set; }
+
+        public string IncidentDate { get; set; } = string.Empty;
+    }
+}
