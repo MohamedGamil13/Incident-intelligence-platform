6ed71991241f412eabe564085d38308cffda6780
Mohamed Gamil <mohamedgamil06616@gmail.com>
2026-09-18 10:18:05 +0300

    update runtime logs gitignore And push Again

---
+++ Incident intelligence platform/Core/Domain/Entities/Logs/Log.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities.Services;
+using Domain.Enums.Logs;
+
+namespace Domain.Entities.Logs
+{
+    public class Log
+    {
+        public int Id { get; set; } //Primary Key only For DB 
+        public LogLevel LogLevel { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; }
+        public Guid TraceId { get; set; } // Will Be Genrated By TraceId Middleware
+        public int ServiceId { get; set; } //Forign key For Service Table (1-M)
+        public Service Service { get; set; }  // Navigation Proprity (In Case i wanna To get a Service By it's Logs ) 
+
+
+    }
+}
+//Models => Create LogModel and it's Relations , Create it's Config Class and Register it in onModelCreateing and add migration and Update DB and Ensure it's Updated Correct ,Handle Indexing
+//Genrate TraceId Middleware
+
+
+//New entity: Log — Timestamp, Level, Message, ServiceId, TraceId.
--- Incident intelligence platform/Incident intelligence platform/Configurations/SerilogConfigs.cs
+++ Incident intelligence platform/Incident intelligence platform/Configurations/SerilogConfigs.cs
@@ -9,7 +9,7 @@ namespace Incident_intelligence_platform.Config
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Information()
                 .WriteTo.Console()
-                .WriteTo.File("logs/myapp-.txt", rollingInterval: RollingInterval.Day)
+                .WriteTo.File("RunTimelogs/myapp-.txt", rollingInterval: RollingInterval.Day)
                 .WriteTo.Seq("http://localhost:5341")
                 .CreateLogger();
 
